b5c7ffb0a01532c1fd7689615b361387488a826a
Bjorn-Olav Strand <bo@startsiden.no>
2010-10-23 17:26:39 +0700

    Some tests working

--- lib/Catalyst/View/Haml.pm
+++ lib/Catalyst/View/Haml.pm
@@ -39,14 +39,44 @@ Catalyst::View::Haml - Haml View Class
 
 sub new {
     my ( $class, $c, $arguments ) = @_;
+    my $config = {
+        TEMPLATE_EXTENSION => '',
+        %{ $class->config },
+        %{$arguments},
+    };
+
+    if ( ! (ref $config->{include_path} eq 'ARRAY') ) {
+        my $delim = $config->{DELIMITER};
+        my @include_path
+            = _coerce_paths( $config->{include_path}, $delim );
+        if ( !@include_path ) {
+            my $root = $c->config->{root};
+            my $base = Path::Class::dir( $root, 'base' );
+            @include_path = ( "$root", "$base" );
+        }
+        $config->{include_path} = \@include_path;
+    }
 
     my $self = $class->next::method(
-        $c, { },
+        $c, { %$config },
     );
+    $self->config($config);
     
     $self->{haml} = Text::Haml->new;
 
     return $self;
+}
+
+sub _coerce_paths {
+    my ( $paths, $dlim ) = shift;
+    return () if ( !$paths );
+    return @{$paths} if ( ref $paths eq 'ARRAY' );
+
+    # tweak delim to ignore C:/
+    unless ( defined $dlim ) {
+        $dlim = ( $^O eq 'MSWin32' ) ? ':(?!\\/)' : ':';
+    }
+    return split( /$dlim/, $paths );
 }
 
 sub process {
@@ -59,11 +89,53 @@ sub process {
         $c->log->debug('No template specified for rendering') if $c->debug;
         return 0;
     }
-
-    my $output = $self->{haml}->render_file($haml, %{ $c->stash() });
+    
+    my $output = $self->render($c, $haml);
+    
     $c->response->body($output);
 
-    return 1;
+    return 1;            
+}
+
+sub render {
+    my ( $self, $c, $haml, $args ) = @_;
+    
+    my $vars = {
+        (ref $args eq 'HASH' ? %$args : %{ $c->stash() }),
+        $self->template_vars($c)
+    };
+
+    if (ref $haml eq 'SCALAR') {
+        my $output = $self->{haml}->render($$haml, %{ $vars });
+        return $output;
+    }
+
+    foreach my $dir (@{$self->config->{include_path}}) {
+        my $file = File::Spec->catfile($dir, $haml);
+        if (-e $file ) {
+            
+            my $output = $self->{haml}->render_file($file, %{ $vars });
+            return $output;
+        }
+    }
+    $c->error("file error - ". $haml .": not found");
+    return "file error - ". $haml .": not found";
+}
+
+sub template_vars {
+    my ( $self, $c ) = @_;
+
+    return  () unless $c;
+    my $cvar = $self->config->{CATALYST_VAR};
+
+    my %vars = defined $cvar
+      ? ( $cvar => $c )
+      : (
+        c    => $c,
+        base => $c->req->base,
+        name => $c->config->{name}
+      );
+      return %vars;
 }
 
 
